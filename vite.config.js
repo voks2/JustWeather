ea6d1c2a1366a29529bb451ad1c3635aad327140
Vojin Mitrovic <vojin.mitrovic@gmail.com>
2025-01-10 03:37:51 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/JustWeather/',
     build: {
     outDir: 'dist', // Specifies the output directory for the built files
   },
