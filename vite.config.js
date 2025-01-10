949c3a011010fdf44a2ae411c40071133dde966b
Vojin Mitrovic <vojin.mitrovic@gmail.com>
2025-01-10 04:21:02 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    define: {
+        'process.env': process.env,
+      },    
     build: {
     outDir: 'dist', // Specifies the output directory for the built files
+    
   },
 });
