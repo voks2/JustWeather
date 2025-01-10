e5465dd0fccd5d032fb37f6148537ae56ee6ba58
Vojin Mitrovic <vojin.mitrovic@gmail.com>
2025-01-10 04:34:03 +0100

    w

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 
+console.log('VITE_OPENWEATHER_API_KEY:', process.env.VITE_OPENWEATHER_API_KEY);  // Debugging line
+
 export default defineConfig({
     base: 'JustWeather',
     define: {
         'process.env': process.env,
-      },    
+    },    
     build: {
-    outDir: 'dist', // Specifies the output directory for the built files
-    
-  },
+        outDir: 'dist', // Specifies the output directory for the built files
+    },
 });
